f5f643cc9829259847b0bbb3382b33a503d12fbd
night.dev <night.dev@hotmail.com>
2025-07-03 20:59:25 -0500

    Update bot.test.js

--- tests/bot.test.js
+++ tests/bot.test.js
@@ -4,7 +4,7 @@ const config = require('../config.json')
 test('should login and emit client.on', async () => {
     const on = jest.fn(() => true);
     const client = new Client({ intents: []})
-     if (client.login(config.TOKEN)) {
+     if (client.login(process.env.TOKEN)) {
         on()
      }
     
