0037aaa2c1d118a2d776d9b66cde2ce39be0ae05
night.dev <night.dev@hotmail.com>
2025-07-03 20:59:42 -0500

    Update mongodb.test.js

--- tests/mongodb.test.js
+++ tests/mongodb.test.js
@@ -3,7 +3,7 @@ const config = require('../config.json')
 
 test('db connnects to discord bot', () => {
     const connected = jest.fn()
-    if (mongoose.connect(config.MONGOURL)) {
+    if (mongoose.connect(process.env.MONGOURL)) {
         connected()
     }
     expect(connected).toHaveReturnedTimes(1)
